af217af94240302d5f201f4f121c68bd4e15eb39
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-12-03 17:10:52 +0700

    update

--- detection/load_detector.py
+++ detection/load_detector.py
@@ -2,13 +2,15 @@ from pathlib import Path
 import torch
 import os
 
-
 def load_yolo(which):
-    """Load a yolo network from local repository. Download the weights there if needed."""
+    """Load a YOLO network from local repository. Download the weights there if needed."""
 
     cwd = Path.cwd()
     yolo_dir = str(Path(__file__).parent.joinpath("yolov5"))
     os.chdir(yolo_dir)
-    model = torch.hub.load(yolo_dir, which, source="local")
+
+    # Thêm trust_repo=True để tránh lỗi weights_only với PyTorch >=2.6
+    model = torch.hub.load(yolo_dir, which, source="local", trust_repo=True)
+
     os.chdir(str(cwd))
     return model
